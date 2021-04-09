d3bcbb1ee5f88ff01fe93b181c517d48a2dcc02e
zlic <161250197@smail.nju.edu.cn>
2021-04-09 20:17:39 +0800

    ✨feat(vuex): 添加 灯泡相关 信息

--- IOT-frontend/src/store/index.js
+++ IOT-frontend/src/store/index.js
@@ -4,11 +4,21 @@ import Vuex from 'vuex';
 
 const store = new Vuex.Store({
     state: {
-        title: 'IOT 设备管理'
+        title: 'IOT 设备管理',
+        lamps: []
     },
     mutations: {
         setTitle (state, title) {
             state.title = title;
+        },
+        setLamps (state, lamps) {
+            state.lamps = [...lamps];
+        }
+    },
+    actions: {
+        async initLamps ({ commit }) {
+            // TODO 获取灯泡数据
+            commit('setLamps', []);
         }
     }
 });
