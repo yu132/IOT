9b810bc343ea471eac5fb32e6b4fb130d8d14b60
zlic <161250197@smail.nju.edu.cn>
2021-04-10 00:15:36 +0800

    ✨feat(数据图表): 添加数据图表超链接组件

---
+++ IOT-frontend/src/components/DataChart/components/DataChartLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <a :href="href">
+      {{ name }}
+    </a>
+  </div>
+</template>
+
+<script>
+// 数据图表-数据图表超链接
+export default {
+  name: "DataChart-DataChartLink",
+  props: {
+    href: {
+      type: String,
+      require: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- IOT-frontend/src/components/DataChart/index.vue
+++ IOT-frontend/src/components/DataChart/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>DataChart</div>
+  <div>
+    DataChart
+    <DataChartLink
+      v-for="{ name, href } in dataChartInfos"
+      :key="name"
+      :name="name"
+      :href="href"
+    />
+  </div>
 </template>
 
 <script>
 // 数据图表
+import { mapState } from "vuex";
+import DataChartLink from "./components/DataChartLink";
+
 export default {
   name: "DataChart",
+  components: {
+    DataChartLink,
+  },
+  computed: {
+    ...mapState(["dataChartInfos"]),
+  },
 };
 </script>
 
