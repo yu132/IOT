afdfd91f240a5cfb423614ec5b8c0477b052152f
zlic <161250197@smail.nju.edu.cn>
2021-04-09 19:36:45 +0800

    ✨feat(前端): 添加 vuex 依赖

--- IOT-frontend/src/main.js
+++ IOT-frontend/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import Vuex from 'vuex';
 
+Vue.use(Vuex);
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
