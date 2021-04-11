a7b15f9063a0c99c4b8b6298c155e0b11ae76e02
zlic <161250197@smail.nju.edu.cn>
2021-04-11 19:34:45 +0800

    ✨feat(api): 添加灯具控制相关接口

--- IOT-frontend/src/api/index.js
+++ IOT-frontend/src/api/index.js
@@ -1,11 +1,53 @@
 import { instance } from '../util/request';
 import { consts } from '../util/consts';
 const {
+    onUrl,
+    offUrl,
+    brightnessUrl,
+    colorUrl,
+    disconnectUrl,
     getLampsUrl,
     getDataChartInfoUrl
 } = consts;
 
 const api = {
+    /**
+     * 打开灯具
+     * @param {*} lampId 
+     */
+    on (lampId) {
+        return instance.post(onUrl, { lampId });
+    },
+    /**
+     * 关闭灯具
+     * @param {*} lampId 
+     */
+    off (lampId) {
+        return instance.post(offUrl, { lampId });
+    },
+    /**
+     * 调整亮度
+     * @param {String} lampId 
+     * @param {Number} brightness 1-100
+     */
+    brightness (lampId, brightness) {
+        return instance.post(brightnessUrl, { lampId, brightness });
+    },
+    /**
+     * 调整颜色
+     * @param {String} lampId 
+     * @param {Number} color 0-2
+     */
+    color (lampId, color) {
+        return instance.post(colorUrl, { lampId, color });
+    },
+    /**
+     * 断开连接
+     * @param {*} lampId 
+     */
+    disconnect (lampId) {
+        return instance.post(disconnectUrl, { lampId });
+    },
     /**
      * 获取灯泡数据
      * TODO 添加更多灯泡属性
