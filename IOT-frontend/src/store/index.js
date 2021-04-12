af570b329759f07628e49246056f0fe5a7879325
zlic <161250197@smail.nju.edu.cn>
2021-04-12 15:11:38 +0800

    🐞fix(vuex): 移除忘记移除的数据图表相关 store

--- IOT-frontend/src/store/index.js
+++ IOT-frontend/src/store/index.js
@@ -7,7 +7,6 @@ const store = new Vuex.Store({
     state: {
         menuIndex: consts.deviceManageMenuIndex,
         lamps: [],
-        dataChartInfos: []
     },
     mutations: {
         setMenuIndex (state, menuIndex) {
@@ -16,25 +15,17 @@ const store = new Vuex.Store({
         setLamps (state, lamps = []) {
             state.lamps = [...lamps];
         },
-        setDataChartInfos (state, dataChartInfos = []) {
-            state.dataChartInfos = [...dataChartInfos];
-        }
     },
     actions: {
         async initStore ({ dispatch }) {
             Promise.all([
                 dispatch('initLamps'),
-                dispatch('initDataChartInfos')
             ]);
         },
         async initLamps ({ commit }) {
             const { data } = await api.getLamps();
             commit('setLamps', data);
         },
-        async initDataChartInfos ({ commit }) {
-            const { data } = await api.getDataChartInfo();
-            commit('setDataChartInfos', data);
-        }
     }
 });
 
