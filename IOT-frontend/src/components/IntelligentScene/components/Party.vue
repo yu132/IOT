ec1b97f0decf529d6db1aa0f4cf52b9917a6230e
zlic <161250197@smail.nju.edu.cn>
2021-04-13 11:40:25 +0800

    ✨feat(智慧场景): Party智慧场景添加设置弹窗

--- IOT-frontend/src/components/IntelligentScene/components/Party.vue
+++ IOT-frontend/src/components/IntelligentScene/components/Party.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="card">
-    <i class="el-icon-goblet-full top-i" />
+    <i class="el-icon-goblet-full top-i" @click="onPartyClick" />
     <span>Party!</span>
-    <i class="el-icon-setting setting" />
+    <i class="el-icon-setting setting" @click="onSettingClick" />
+    <CheckBoxDialog
+      :ref="dialogRef"
+      :title="dialogTitle"
+      :checkedOptionIds="partyLampIds"
+      :options="lamps"
+      :beforeCloseCallback="updatePartyLampIds"
+    />
   </div>
 </template>
 
 <script>
 // 智慧场景-派对模式
+import CheckBoxDialog from "./CheckBoxDialog";
+import { mapState, mapActions } from "vuex";
+
 export default {
   name: "IntelligentScene_Party",
+  components: {
+    CheckBoxDialog,
+  },
+  computed: {
+    ...mapState(["lamps", "partyLampIds"]),
+  },
+  data() {
+    return {
+      dialogTitle: "请选择Party相关灯具设备",
+      dialogRef: "dialogRef",
+    };
+  },
+  methods: {
+    ...mapActions(["updatePartyLampIds"]),
+    onPartyClick() {
+      // TODO
+    },
+    onSettingClick() {
+      this.$refs[this.dialogRef].showDialog();
+    },
+  },
 };
 </script>
 
