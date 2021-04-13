63ef08bd65d2b91d9c99d7046a0dc24ff68cf2bf
zlic <161250197@smail.nju.edu.cn>
2021-04-13 11:37:35 +0800

    ✨feat(智慧场景): 离家智慧场景添加设置弹窗

--- IOT-frontend/src/components/IntelligentScene/components/LeaveHome.vue
+++ IOT-frontend/src/components/IntelligentScene/components/LeaveHome.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="card">
-    <i class="el-icon-house top-i" />
+    <i class="el-icon-house top-i" @click="onLeaveHomeClick" />
     <span>离家</span>
-    <i class="el-icon-setting setting" />
+    <i class="el-icon-setting setting" @click="onSettingClick" />
+    <CheckBoxDialog
+      :ref="dialogRef"
+      :title="dialogTitle"
+      :checkedOptionIds="leaveHomeLampIds"
+      :options="lamps"
+      :beforeCloseCallback="updateLeaveHomeLampIds"
+    />
   </div>
 </template>
 
 <script>
 // 智慧场景-离家
+import CheckBoxDialog from "./CheckBoxDialog";
+import { mapState, mapActions } from "vuex";
+
 export default {
   name: "IntelligentScene_LeaveHome",
+  components: {
+    CheckBoxDialog,
+  },
+  computed: {
+    ...mapState(["lamps", "leaveHomeLampIds"]),
+  },
+  data() {
+    return {
+      dialogTitle: "请选择离家相关灯具设备",
+      dialogRef: "dialogRef",
+    };
+  },
+  methods: {
+    ...mapActions(["updateLeaveHomeLampIds"]),
+    onLeaveHomeClick() {
+      // TODO
+    },
+    onSettingClick() {
+      this.$refs[this.dialogRef].showDialog();
+    },
+  },
 };
 </script>
 
