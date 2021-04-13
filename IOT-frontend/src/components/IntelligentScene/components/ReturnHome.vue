f14d9f03c1dfe59a29a5290960f172e9950b3cd2
zlic <161250197@smail.nju.edu.cn>
2021-04-13 11:42:04 +0800

    ✨feat(智慧场景): 归家智慧场景添加设置弹窗

--- IOT-frontend/src/components/IntelligentScene/components/ReturnHome.vue
+++ IOT-frontend/src/components/IntelligentScene/components/ReturnHome.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="card">
-    <i class="el-icon-s-home top-i" />
+    <i class="el-icon-s-home top-i" @click="onReturnHomeClick" />
     <span>归家</span>
-    <i class="el-icon-setting setting" />
+    <i class="el-icon-setting setting" @click="onSettingClick" />
+    <CheckBoxDialog
+      :ref="dialogRef"
+      :title="dialogTitle"
+      :checkedOptionIds="returnHomeLampIds"
+      :options="lamps"
+      :beforeCloseCallback="updateReturnHomeLampIds"
+    />
   </div>
 </template>
 
 <script>
 // 智慧场景-归家
+import CheckBoxDialog from "./CheckBoxDialog";
+import { mapState, mapActions } from "vuex";
+
 export default {
   name: "IntelligentScene_ReturnHome",
+  components: {
+    CheckBoxDialog,
+  },
+  computed: {
+    ...mapState(["lamps", "returnHomeLampIds"]),
+  },
+  data() {
+    return {
+      dialogTitle: "请选择离家相关灯具设备",
+      dialogRef: "dialogRef",
+    };
+  },
+  methods: {
+    ...mapActions(["updateReturnHomeLampIds"]),
+    onReturnHomeClick() {
+      // TODO
+    },
+    onSettingClick() {
+      this.$refs[this.dialogRef].showDialog();
+    },
+  },
 };
 </script>
 
